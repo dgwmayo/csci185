2732ae08455aa304394e421d2707cb788c5d3899
Dylan <dmayo@unca.edu>
2024-03-21 11:05:13 -0400

    1st Deliverable

---
+++ lectures/lecture14/01-greeting/greeting.js
@@ -0,0 +1,12 @@
+function sayHello() {
+    const myElement = document.querySelector("#message") ; 
+    myElement.innerHTML = "hello junkie";
+    // document.querySelector('#message').innerHTML = '<p>hello junkie</p>';
+}
+
+function sayGoodbye() {
+    const myElement = document.querySelector("#message") ; 
+    myElement.innerHTML = "sell your soul to krona";
+    // document.querySelector('#message').innerHTML = '<p>sell your soul to krona and repent before the end</p>';
+}
+
